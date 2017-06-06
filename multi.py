08d885708e283ab303eb66b659c7c20d90c05285
Jon Pinnow <Jon@main.TECHVITALITY.com>
2017-06-06 16:40:53 -0400

    Made a new function to contain watermark task

--- multi.py
+++ multi.py
@@ -21,11 +21,9 @@ sys.stdout.write("Copy Progress: [%s]" % (" " * TOOLBAR_WIDTH))
 sys.stdout.flush()
 sys.stdout.write("\b" * (TOOLBAR_WIDTH+1))
 # setting up all variables
-# sets OPACITY of the water mark
-OPACITY = 1
+
 # AMOUNT = 500
-# this is the predicted width of the water mork text
-TEXT_WIDTH = 400
+
 # selecting the file that is a jpeg amoung other FILES
 IMG_FILE = FILES[img_location]
 # Getting the last occurrence of the period to get file EXTENSION
@@ -35,28 +33,29 @@ NAME = IMG_FILE[:LAST_DOT]
 EXTENSION = IMG_FILE[LAST_DOT+1:]
 # opens image file
 BASE = Image.open(IMG_FILE).convert('RGBA')
-STEP = (BASE.size[0] - TEXT_WIDTH) / AMOUNT
-# make a blank image for the text, initialized to transparent text color
-TXT = Image.new('RGBA', BASE.size, (255,255,255,0))
 
-# get a font
-FNT = ImageFont.truetype('arial.ttf', 60)
-# new drawing object
-d = ImageDraw.Draw(TXT)
+
+def multi_watermark(amount, base_image, txt='SaveMyServer'):
+    '''
+    Takes an image and makes multiple copies with a watermark
+    '''
+    # get a font
+    fnt = ImageFont.truetype('arial.ttf', 60)
+    # sets OPACITY of the water mark
+    opacity = 1
+    # this is the predicted width of the water mork text
+    text_width = 400
+    # The increment the watermark is moved for each image
+    step = (base_image.size[0] - text_width) / amount
+    position = 0
+    # Loop to make multiple images
+    for i in xrange(amount):
+        position += step
+        txt = Image.new('RGBA', base_image.size, (255, 255, 255, 0))
+        d = ImageDraw.Draw(txt)
+        d.text((position, 10), "SaveMyServer", font=fnt, fill=(0, 0, 0, opacity))
+        out = Image.alpha_composite(base_image, txt)
+        out.save(NAME + "-watermarked-" + str(i + 1)+ "." + EXTENSION)
 
 if __name__ == '__main__':
-    # Loop that creates each unique photo
-    POSITION = 0
-    for i in range(AMOUNT):
-        POSITION += STEP
-        d.text((POSITION, 10), "SaveMyServer", font=FNT, fill=(0, 0, 0, OPACITY))
-        out = Image.alpha_composite(BASE, TXT)
-        out.save(NAME + "-watermarked-" + str(i + 1)+ "." + EXTENSION)
-        TXT = Image.new('RGBA', BASE.size, (255, 255, 255, 0))
-        d = ImageDraw.Draw(TXT)
-        if i % AMOUNT // TOOLBAR_WIDTH == 0:
-            for i in range((TOOLBAR_WIDTH // AMOUNT) + (TOOLBAR_WIDTH % AMOUNT)):
-                sys.stdout.write("#")
-                sys.stdout.flush()
-
-    sys.stdout.write("\n")
+    multi_watermark(AMOUNT, BASE)
