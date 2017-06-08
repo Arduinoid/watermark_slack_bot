88d4e4c8f1291cdb04cfcc7e5eea6c80424870ab
Jon Pinnow <Jon@main.TECHVITALITY.com>
2017-06-08 13:03:09 -0400

    Inlcuding powershell deployment script

---
+++ Deploy-SlackBot.ps1
@@ -0,0 +1,28 @@
+function Deploy-SlackBot ([switch]$Start, [switch]$NoCopy) {
+    $source = '\\JON-T3600\C$\Deployment\Slack_Bot'
+    $destination = '\\NAS-R510\C$\Users\Jon\Documents\Slack'
+    $server = 'NAS-R510'
+    if ((Test-Path $source) -and -not $NoCopy) {
+        Write-Host "Beginning to copy files to server..." -ForegroundColor Cyan
+        Copy-Item -Path $source -Destination $destination -Force -Recurse -Confirm
+        Write-Host "Finished copy process" -ForegroundColor Cyan
+    }
+    if ($Start) {
+        if ((Get-Process -ComputerName $server -Name pythonw -ErrorAction SilentlyContinue)) {
+            Write-Host 'Slack Bot was running and will now be stopped...' -ForegroundColor Yellow
+            Invoke-Command $server {
+                Stop-Process -Name pythonw
+                Start-Sleep -Seconds 1
+                Start-ScheduledTask -TaskPath \ -TaskName 'Slack Bot'
+            }
+        }
+        else {
+            Invoke-Command $server {
+                Start-ScheduledTask -TaskPath \ -TaskName 'Slack Bot'
+            }
+        }
+        $process = (Get-Process -ComputerName $server -Name pythonw).id
+        Write-Host "Slack Bot should now be running on the following process: $process" -ForegroundColor Green
+        
+    }
+}
