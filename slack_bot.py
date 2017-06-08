8d3a8a06b65e02f360e1dc9d9340bb346c4cad58
Jon Pinnow <Jon@main.TECHVITALITY.com>
2017-06-08 11:37:09 -0400

    Made function to convert user id to user name. Bot now mentions to user who initiates the make request upon completion.

--- slack_bot.py
+++ slack_bot.py
@@ -18,7 +18,7 @@ COMMANDS = {'make': 0}
 slack_client = SlackClient(SLACK_TOKEN)
 
 
-def handle_command(command, channel):
+def handle_command(command, channel, user):
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
@@ -31,25 +31,20 @@ def handle_command(command, channel):
                Example: `@waterboy make 500` will make 500 watermarked images"""
     # Determine which command the user is requesting to use
     if command.startswith('make'):
-        print('make command issued')
         ex = r'\d+'
         match = re.search(ex, command)
         amount = match.group(0)
         if int(amount) <= 700:
-            print('correct number passed')
             image_files = get_image_files(LOCATION)
             img_file = pick_image_from(image_files)
-            print(img_file[0] == None)
             if img_file[0] is not None:
-                print('image file not None')
                 out_directory = img_file[1] + 'watermarked_' + img_file[0].split('.')[0]
                 response = "Alright, I'm on it. Making " + amount + " copies of " + img_file[0] + " image file..."
                 try:
-                    print('trying to make stuff')
                     os.mkdir(out_directory)
                     slack_client.api_call("chat.postMessage", channel=channel,text=response, as_user=True)
                     multi_watermark(img_file,int(amount))
-                    response = "OK, you're all set @channel"
+                    response = "OK, you're all set @{}".format(get_user_name(user))
                 except:
                     response = "Oops, looks like there's already a folder here with these pictures\nTry removing the old folder first and run the command again. :ghost:"
                 slack_client.api_call("chat.postMessage", channel=channel,text=response, as_user=True)
@@ -75,8 +70,16 @@ def parse_slack_output(slack_rtm_output):
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
-    return None, None
+                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel'], output['user']
+    return None, None, None
+
+
+def get_user_name(user_id):
+    users = (slack_client.api_call('users.list')).get('members')
+    for i in users:
+        if i['id'] == user_id:
+            return i['name']
+    return None
 
 
 if __name__ == '__main__':
@@ -84,9 +87,9 @@ if __name__ == '__main__':
     if slack_client.rtm_connect():
         print("multi_watermark bot connected and running")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
+            command, channel, user = parse_slack_output(slack_client.rtm_read())
             if command and channel:
-                handle_command(command, channel)
+                handle_command(command, channel, user)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection faild. Invalid Slack token or bot ID?")
