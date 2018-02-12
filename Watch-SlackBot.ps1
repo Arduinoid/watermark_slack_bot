75c60e0504b0d91d998d81d3e576ffe479b68792
Jonathon Pinnow <pinnowj@gmail.com>
2018-02-12 15:32:48 -0500

    Change the watcher script to check if the log file exists

--- Watch-SlackBot.ps1
+++ Watch-SlackBot.ps1
@@ -1,7 +1,9 @@
+$log_file = 'C:\Scripts\slack_bot_log.txt'
+if (-not (test-path $log_file)) {  New-Item $log_file}
 if(-not (gps pythonw)) {
     Start-ScheduledTask -TaskPath \ -TaskName 'Slack Bot'
-    Write-Output "Slack Bot restarted on: $(date)" >> 'C:\Scripts\slack_bot_log.txt'
+    Write-Output "Slack Bot restarted on: $(date)" >> $log_file
 }
 else {
-    Write-Output "Slack Bot is still running: $(date)" >> 'C:\Scripts\slack_bot_log.txt'
+    Write-Output "Slack Bot is still running: $(date)" >> $log_file
 }
